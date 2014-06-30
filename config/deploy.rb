d3927d59f311593ca825a310415bc88cb803747a
Andrei Zaika <anzaika@gmail.com>
2014-06-30 21:24:34 +0400

    prepare for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,52 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'oncodb'
+set :deploy_user, 'deploy'
+
+set :scm, :git
+set :repo_url, 'git@github.com:anzaika/oncodb.git'
+
+set :rbenv_type, :system
+set :rbenv_ruby, '2.1.2'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
+set :keep_releases, 5
+
+set :linked_files, %w{config/database.yml config/secrets.yml}
+
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :tests, ["spec"]
+
+set(:config_files, %w(
+    nginx.conf
+    database.yml
+    secrets.yml
+    monit
+    unicorn.rb
+    unicorn_init.sh
+))
+
+set(:executable_config_files, %w(
+    unicorn_init.sh
+))
+
+set(:symlinks, [
+  {
+    source: 'nginx.conf',
+    link: "/etc/nginx/sites-enabled/{{full_app_name}}"
+  },
+  {
+    source: 'unicorn_init.sh',
+    link: "/etc/init.d/unicorn_{{full_app_name}}"
+  },
+  {
+    source: 'monit',
+    link: "/etc/monit/conf.d/{{full_app_name}}.conf"
+  }
+])
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
 
 # set :format, :pretty
 # set :log_level, :debug
@@ -18,23 +60,9 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+  # before :deploy, 'deploy:run_tests'
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
   after :finishing, 'deploy:cleanup'
 
 end
