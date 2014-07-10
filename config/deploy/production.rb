61df6da508339e93bfea38be6071a1587d1c9aba
Andrei Zaika <anzaika@gmail.com>
2014-07-10 10:43:48 +0400

    fix production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :branch, 'master'
 set :server_name, "www.oncodb.labhub.co oncodb.labhub.co"
 
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
-server '95.85.24.49', user: 'deploy', roles: %w{web app}, primary: true
+server '146.185.148.177', user: 'deploy', roles: %w{web app}, primary: true
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
 set :rails_env, :production
